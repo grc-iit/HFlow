52696c843c205c6593c992c9515e4fb54b3ca526
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-14 17:03:52 -0500

    initial commit.

---
+++ include/rhea/rhea.h
@@ -0,0 +1,15 @@
+//
+// Created by mani on 9/14/2020.
+//
+
+#ifndef RHEA_RHEA_H
+#define RHEA_RHEA_H
+
+namespace rhea{
+class client {
+
+};
+}
+
+
+#endif //RHEA_RHEA_H
---
+++ src/rhea/server/server.h
@@ -0,0 +1,16 @@
+//
+// Created by mani on 9/14/2020.
+//
+
+#ifndef RHEA_JOB_MANAGER_H
+#define RHEA_SERVER_H
+
+namespace rhea{
+    class server {
+
+    };
+}
+
+
+
+#endif //RHEA_JOB_MANAGER_H
