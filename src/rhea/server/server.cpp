45f77a7dd78a459030d32b3cfd6aaef0b963ac68
Neeraj Rajesh <neeraj@room429>
2020-09-16 17:03:46 -0500

    update no test

--- src/rhea/server/server.cpp
+++ src/rhea/server/server.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "server.h"
+
+void rhea::server::run() {
+	std::promise<void> exit_signal;
+	std::future<void> future_obj = exit_signal.get_future();
+	std::thread t_thread(&rhea::server::single_loop, this, std::move(future_obj));
+	// promises.push_back(std::move(exit_signal));
+	// pool.push_back(std::move(t_thread));
+
+	t_thread.join();
+}
+
+uint_fast64_t rhea::server::get_in_rate(){
+	return 10;
+}
+
+uint_fast64_t rhea::server::get_out_rate(){
+	return 10;
+}
+
+int rhea::server::alter_collector(rhea::Alter_Type){
+	// doOp
+	return 1;
+}
+int rhea::server::alter_transformers(rhea::Alter_Type){
+	//doOp
+	return 1;
+}
+int rhea::server::alter_writers(rhea::Alter_Type){
+	//doOp
+	return 1;
+}
+
+void rhea::server::single_loop(std::future<void> futureObj) {
+	while (futureObj.wait_for(std::chrono::microseconds(interval)) == std::future_status::timeout) {
+		auto in_rate = get_in_rate();
+		auto out_rate = get_out_rate();
+		if (abs(out_rate - in_rate) >= variation){
+			if (in_rate > out_rate){
+				auto op = Alter_Type::GROW;
+				auto ret = alter_writers(op);
+			}
+			else if (out_rate >  in_rate){
+				auto op = Alter_Type::SHRINK;
+				auto ret = alter_writers(op);
+			}
+		}
+	}
+}
+
