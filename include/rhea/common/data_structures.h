3b2f4ac7d0803f639892b3d5d96e9468420ac2ef
Keith-Bateman <kbateman@hawk.iit.edu>
2020-09-17 08:34:48 -0500

    Added Parcel Structure to Rhea

--- include/rhea/common/data_structures.h
+++ include/rhea/common/data_structures.h
@@ -7,13 +7,27 @@
 
 #include <sentinel/common/data_structures.h>
 
+typedef struct Parcel: public Data{
+
+    /*Define the default, copy and move constructor*/
+    Parcel(CharStruct id, size_t position, char *buffer, uint16_t storage_index, size_t data_size): Data(id_(id), position_(position), buffer_(buffer), storage_index_(storage_index),data_size_(data_size)){}
+
+    Parcel(const Parcel &other): Data(other){}
+    Parcel(Parcel &other): Data(other){}
+    /*Define Assignment Operator*/
+    Parcel &operator=(const Parcel &other){
+        Data::operator=(other);
+        return *this;
+    }
+}Parcel;
+
 typedef struct Flow {
-    std::vector<Data> parcels_;
+    std::vector<Parcel> parcels_;
 
     /*Define the default, copy and move constructor*/
     Flow(): parcels_(){}
-    Flow(const Flow &other): parcels_(other.parcels_.copy()){}
-    Flow(Flow &other): parcels_(other.parcels_.copy()){}
+    Flow(const Flow &other): parcels_(std::vector(other.parcels_)){}
+    Flow(Flow &other): parcels_(std::vector(other.parcels_)){}
 } Flow;
 
 #endif //RHEA_DATA_STRUCTURES_H
