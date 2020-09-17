94d983b9f8f5b9240ec958f048359ede0bfcbee6
Keith-Bateman <kbateman@hawk.iit.edu>
2020-09-17 08:05:04 -0500

    Added Flow structure to Rhea

--- include/rhea/common/data_structures.h
+++ include/rhea/common/data_structures.h
@@ -5,4 +5,15 @@
 #ifndef RHEA_DATA_STRUCTURES_H
 #define RHEA_DATA_STRUCTURES_H
 
+#include <sentinel/common/data_structures.h>
+
+typedef struct Flow {
+    std::vector<Data> parcels_;
+
+    /*Define the default, copy and move constructor*/
+    Flow(): parcels_(){}
+    Flow(const Flow &other): parcels_(other.parcels_.copy()){}
+    Flow(Flow &other): parcels_(other.parcels_.copy()){}
+} Flow;
+
 #endif //RHEA_DATA_STRUCTURES_H
