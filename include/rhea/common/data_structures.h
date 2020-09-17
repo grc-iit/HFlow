a4720095662f21d77debf4993c2807f2b7930401
Keith-Bateman <kbateman@hawk.iit.edu>
2020-09-17 11:53:07 -0500

    Changed Parcel to use Event structure

--- include/rhea/common/data_structures.h
+++ include/rhea/common/data_structures.h
@@ -7,27 +7,82 @@
 
 #include <sentinel/common/data_structures.h>
 
-typedef struct Parcel: public Data{
+typedef struct Parcel : public Event {
 
     /*Define the default, copy and move constructor*/
-    Parcel(CharStruct id, size_t position, char *buffer, uint16_t storage_index, size_t data_size): Data(id_(id), position_(position), buffer_(buffer), storage_index_(storage_index),data_size_(data_size)){}
+    Parcel(CharStruct id_, size_t position_, char *buffer_, uint16_t storage_index_, size_t data_size_) : Event(id_(id_), position_(position_), buffer_(buffer_), storage_index_(storage_index_), data_size_(data_size_)) {}
 
-    Parcel(const Parcel &other): Data(other){}
-    Parcel(Parcel &other): Data(other){}
+    Parcel(const Parcel &other) : Event(other) {}
+
+    Parcel(Parcel &other) : Event(other) {}
+
     /*Define Assignment Operator*/
-    Parcel &operator=(const Parcel &other){
-        Data::operator=(other);
+    Parcel &operator=(const Parcel &other) {
+        Event::operator=(other);
         return *this;
     }
-}Parcel;
+} Parcel;
 
-typedef struct Flow {
-    std::vector<Parcel> parcels_;
+namespace clmdep_msgpack {
+    MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS) {
+        namespace mv1 = clmdep_msgpack::v1;
 
-    /*Define the default, copy and move constructor*/
-    Flow(): parcels_(){}
-    Flow(const Flow &other): parcels_(std::vector(other.parcels_)){}
-    Flow(Flow &other): parcels_(std::vector(other.parcels_)){}
-} Flow;
+        namespace adaptor {
+            template<>
+            struct convert<Parcel> {
+                mv1::object const &operator()(mv1::object const &o, Parcel &input) const {
+                    input.id_ = o.via.array.ptr[0].as<CharStruct>();
+                    input.position_ = o.via.array.ptr[1].as<size_t>();
+                    auto data = o.via.array.ptr[2].as<std::string>();
+                    input.data_size_ = o.via.array.ptr[3].as<size_t>();
+                    if (!data.empty()) {
+                        input.buffer_ = static_cast<char *>(malloc(input.data_size_));
+                        memcpy(input.buffer_, data.data(), input.data_size_);
+                    }
+                    input.storage_index_ = o.via.array.ptr[4].as<uint16_t>();
+                    return o;
+                }
+            };
+
+            template<>
+            struct pack<Parcel> {
+                template<typename Stream>
+                packer <Stream> &operator()(mv1::packer<Stream> &o, Parcel const &input) const {
+                    o.pack_array(5);
+                    o.pack(input.id_);
+                    o.pack(input.position_);
+                    if (input.buffer_ == NULL) o.pack(std::string());
+                    else {
+                        o.pack(std::string(input.buffer_, input.data_size_));
+                    }
+                    o.pack(input.data_size_);
+                    o.pack(input.storage_index_);
+                    return o;
+                }
+            };
+
+            template<>
+            struct object_with_zone<Parcel> {
+                void operator()(mv1::object::with_zone &o, Parcel const &input) const {
+                    o.type = type::ARRAY;
+                    o.via.array.size = 5;
+                    o.via.array.ptr = static_cast<clmdep_msgpack::object *>(o.zone.allocate_align(
+                            sizeof(mv1::object) * o.via.array.size, MSGPACK_ZONE_ALIGNOF(mv1::object)));
+                    o.via.array.ptr[0] = mv1::object(input.id_, o.zone);
+                    o.via.array.ptr[1] = mv1::object(input.position_, o.zone);
+                    if (input.buffer_ == NULL) o.via.array.ptr[2] = mv1::object(std::string(), o.zone);
+                    else {
+                        o.via.array.ptr[2] = mv1::object(std::string(input.buffer_, input.data_size_), o.zone);
+                        free(input.buffer_);
+                    }
+                    o.via.array.ptr[3] = mv1::object(input.data_size_, o.zone);
+                    o.via.array.ptr[4] = mv1::object(input.storage_index_, o.zone);
+                }
+            };
+        }  // namespace adaptor
+    }
+}  // namespace clmdep_msgpack
+
+std::ostream &operator<<(std::ostream &os, Parcel &parcel);
 
 #endif //RHEA_COMMON_DATA_STRUCTURES_H
