a3ec370a55598cc7cbc8ee95fc89c9842eaa37a8
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-09-20 14:30:13 -0500

    Rhea Test:   - Changes to the nton basic test

--- test/main.cpp
+++ test/main.cpp
@@ -5,13 +5,20 @@
 #include <mpi.h>
 #include <rhea/rhea.h>
 #include <basket/common/singleton.h>
+#include <sentinel/worker_manager/client.h>
+//#include <common/class_loader.h>
 
 int main(int argc, char * argv[]){
     MPI_Init(&argc,&argv);
     MPI_Barrier(MPI_COMM_WORLD);
 
-    uint32_t job_id = 0;
-    rhea::Client client(job_id);
+    uint32_t jobId = 2;
+
+//    COMMON_CONF;
+//    auto classLoader = ClassLoader();
+//    std::shared_ptr<Job<Event>> job = classLoader.LoadClass<Job<Event>>(jobId);
+
+    rhea::Client client(jobId);
 
     Parcel parcel;
     char* data = "   ";
