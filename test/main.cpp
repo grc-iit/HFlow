3dfdb6e3412093e672cb119da7236a9a2438176c
JaimeCernuda <jcernudagarcia@hawk.iit.edu>
2020-09-19 22:14:37 -0500

    Rhea Test:   - Added Simple Rhea test that connects to client and publishes some data.

--- test/main.cpp
+++ test/main.cpp
@@ -2,3 +2,21 @@
 // Created by hdevarajan on 9/19/20.
 //
 
+#include <mpi.h>
+#include <rhea/rhea.h>
+#include <basket/common/singleton.h>
+
+int main(int argc, char * argv[]){
+    MPI_Init(&argc,&argv);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    uint32_t job_id = 0;
+    rhea::Client client(job_id);
+
+    Parcel parcel;
+    char* data = "   ";
+    client.Publish(parcel, data);
+
+
+    MPI_Finalize();
+}
