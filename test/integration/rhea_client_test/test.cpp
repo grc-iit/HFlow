409e7eee634b4e25c947283d2b9c5dc502c6383f
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-25 14:00:25 -0500

    fixed byteflow_regulator test for calculating inflow and outflow rate.

--- test/integration/rhea_client_test/test.cpp
+++ test/integration/rhea_client_test/test.cpp
@@ -110,7 +110,8 @@ int main(int argc, char* argv[]){
     if(BASKET_CONF->MPI_RANK == 0){
         printf("\nAsync Write Time %f, Sync Write time %f\n",write_async_time,writer_timer.getElapsedTime());
     }
-
+    write_client.FinalizeClient();
+    sleep(5);
     //Read from file
     rhea::Client read_client(1);
     auto read_parcels = std::vector<Parcel>();
@@ -142,7 +143,6 @@ int main(int argc, char* argv[]){
 
 
     //Finalize
-    write_client.FinalizeClient();
     read_client.FinalizeClient();
     MPI_Finalize();
 }
