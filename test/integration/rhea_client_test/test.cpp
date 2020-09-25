8a1f30cb8052231fa378014f88035e62e9357666
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-25 14:28:53 -0500

    fixed byteflow_regulator test for calculating inflow and outflow rate.

--- test/integration/rhea_client_test/test.cpp
+++ test/integration/rhea_client_test/test.cpp
@@ -33,7 +33,7 @@ public:
 
     TestArgs(int argc, char **argv) {
         AddOpt("-c", common::args::ArgType::kInt, 1);
-        AddOpt("-bs", common::args::ArgType::kSize, 4*(1ul<<10));
+        AddOpt("-bs", common::args::ArgType::kSize, 4 * (1ul << 10));
         AddOpt("-rc", common::args::ArgType::kInt, 1);
         AddOpt("-wc", common::args::ArgType::kInt, 1);
         AddOpt("-base", common::args::ArgType::kString, "/tmp");
@@ -46,16 +46,16 @@ public:
 #define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 #define PBWIDTH 60
 
-void printProgress(double percentage,char* data="") {
+void printProgress(double percentage, char *data = "") {
     int val = (int) (percentage * 100);
     int lpad = (int) (percentage * PBWIDTH);
     int rpad = PBWIDTH - lpad;
-    printf("\r%s %3d%% [%.*s%*s]",data, val, lpad, PBSTR, rpad, "");
+    printf("\r%s %3d%% [%.*s%*s]", data, val, lpad, PBSTR, rpad, "");
     fflush(stdout);
 }
 
-int main(int argc, char* argv[]){
-    MPI_Init(&argc,&argv);
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
     int rank;
 
     //Get the number of clients
@@ -67,8 +67,8 @@ int main(int argc, char* argv[]){
     int wc = args.GetIntOpt("-wc");
     int clients_per_file = args.GetIntOpt("-c");
     size_t bs = args.GetSizeOpt("-bs");
-    int sink_group = rank/clients_per_file;
-    int sink_member = rank%clients_per_file;
+    int sink_group = rank / clients_per_file;
+    int sink_member = rank % clients_per_file;
     std::string base = args.GetStringOpt("-base") + "/test" + std::to_string(sink_group) + ".txt";
 
     auto conf = args.GetStringOpt("-conf");
@@ -79,75 +79,77 @@ int main(int argc, char* argv[]){
     RHEA_CONF->LoadConfiguration();
 
     //Allocate block
-    char *data = (char*)malloc(bs);
+    char *data = (char *) malloc(bs);
     memset(data, 'a', bs);
-    char *data_save = (char*)malloc(bs);
+    char *data_save = (char *) malloc(bs);
     memset(data_save, 'a', bs);
 
     //Write to file
-    rhea::Client write_client(0);
-    auto write_parcels = std::vector<Parcel>();
-    auto writer_timer = Timer();
-    writer_timer.resumeTime();
-    for(int i = 0; i < wc; ++i) {
-        Parcel parcel;
-        parcel.id_ = base;
-        parcel.storage_index_ = 0;
-        parcel.type_ = OperationType::PUBLISH;
-        parcel.position_ = sink_member*bs*wc + i*bs;
-        parcel.data_size_ = bs;
-        write_client.Publish(parcel, data);
-        write_parcels.push_back(parcel);
+    if(wc > 0){
+        rhea::Client write_client(0);
+        auto write_parcels = std::vector<Parcel>();
+        auto writer_timer = Timer();
+        writer_timer.resumeTime();
+        for (int i = 0; i < wc; ++i) {
+            Parcel parcel;
+            parcel.id_ = base;
+            parcel.storage_index_ = 0;
+            parcel.type_ = OperationType::PUBLISH;
+            parcel.position_ = sink_member * bs * wc + i * bs;
+            parcel.data_size_ = bs;
+            write_client.Publish(parcel, data);
+            write_parcels.push_back(parcel);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        writer_timer.pauseTime();
+        auto write_async_time = writer_timer.getElapsedTime();
+        writer_timer.resumeTime();
+        for (int i = 0; i < wc; ++i) {
+            printProgress((double(i + 1)) / wc, "write");
+            write_client.Wait(write_parcels[i]);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        writer_timer.pauseTime();
+        if (BASKET_CONF->MPI_RANK == 0) {
+            printf("\nAsync Write Time %f, Sync Write time %f\n", write_async_time, writer_timer.getElapsedTime());
+        }
+        write_client.FinalizeClient();
+        sleep(5);
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-    writer_timer.pauseTime();
-    auto write_async_time = writer_timer.getElapsedTime();
-    writer_timer.resumeTime();
-    for(int i=0;i<wc;++i){
-        printProgress((double (i+1))/wc,"write");
-        write_client.Wait(write_parcels[i]);
+    if (rc > 0) {
+        //Read from file
+        rhea::Client read_client(1);
+        auto read_parcels = std::vector<Parcel>();
+        auto read_timer = Timer();
+        read_timer.resumeTime();
+        for (int i = 0; i < rc; ++i) {
+            Parcel parcel;
+            parcel.id_ = base;
+            parcel.storage_index_ = 0;
+            parcel.type_ = OperationType::SUBSCRIBE;
+            parcel.position_ = sink_member * bs * wc + i * bs;
+            parcel.data_size_ = bs;
+            read_client.AsyncSubscribe(parcel);
+            read_parcels.push_back(parcel);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        read_timer.pauseTime();
+        auto read_async_time = read_timer.getElapsedTime();
+        read_timer.resumeTime();
+        memset(data, 0, bs);
+        for (int i = 0; i < rc; ++i) {
+            printProgress((double(i + 1)) / wc, "read");
+            read_client.Wait(read_parcels[i]);
+            read_client.GetSubscribedData(read_parcels[i], data);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        read_timer.pauseTime();
+        if (BASKET_CONF->MPI_RANK == 0) {
+            printf("\nAsync Read Time %f, Sync Read time %f\n", read_async_time, read_timer.getElapsedTime());
+        }
+        read_client.FinalizeClient();
     }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    writer_timer.pauseTime();
-    if(BASKET_CONF->MPI_RANK == 0){
-        printf("\nAsync Write Time %f, Sync Write time %f\n",write_async_time,writer_timer.getElapsedTime());
-    }
-    write_client.FinalizeClient();
-    sleep(5);
-    //Read from file
-    rhea::Client read_client(1);
-    auto read_parcels = std::vector<Parcel>();
-    auto read_timer = Timer();
-    read_timer.resumeTime();
-    for(int i = 0; i < rc; ++i) {
-        Parcel parcel;
-        parcel.id_ = base;
-        parcel.storage_index_ = 0;
-        parcel.type_ = OperationType::SUBSCRIBE;
-        parcel.position_ = sink_member*bs*wc + i*bs;
-        parcel.data_size_ = bs;
-        read_client.AsyncSubscribe(parcel);
-        read_parcels.push_back(parcel);
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
-    read_timer.pauseTime();
-    auto read_async_time = read_timer.getElapsedTime();
-    read_timer.resumeTime();
-    memset(data, 0, bs);
-    for(int i=0;i<rc;++i){
-        printProgress((double (i+1))/wc,"read");
-        read_client.Wait(read_parcels[i]);
-        read_client.GetSubscribedData(read_parcels[i],data);
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
-    read_timer.pauseTime();
-    if(BASKET_CONF->MPI_RANK == 0){
-        printf("\nAsync Read Time %f, Sync Read time %f\n",read_async_time,read_timer.getElapsedTime());
-    }
-
 
     //Finalize
-    read_client.FinalizeClient();
     MPI_Finalize();
 }
